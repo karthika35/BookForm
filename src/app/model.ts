f77b2883287bb0de40cef8704a9c8f49ee514ad0
Ragulan Gopalapillai <ragulan28@gmail.com>
2019-11-12 10:34:20 +0530

    model added

---
+++ src/app/model.ts
@@ -0,0 +1,12 @@
+
+export interface Book {
+  author: Author;
+  id: string;
+  name: string;
+  price: number;
+}
+
+export interface Author {
+  id: string;
+  name: string;
+}
--- src/app/widget/card/card.component.ts
+++ src/app/widget/card/card.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Book} from '../../Book';
+import {Book} from '../../model';
 
 @Component({
   selector: 'app-card',
